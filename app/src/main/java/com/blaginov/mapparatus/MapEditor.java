70d5d0267a960d5ea3fe16bf63f2e86ed56c80bc
Stefan Blaginov <stefanblag@gmail.com>
2015-03-02 17:11:55 +0000

    Added a button to test things out with

--- app/src/main/java/com/blaginov/mapparatus/MapEditor.java
+++ app/src/main/java/com/blaginov/mapparatus/MapEditor.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MapEditor extends ActionBarActivity {
@@ -35,5 +36,9 @@ public class MapEditor extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startProcess(View view) {
+
     }
 }
