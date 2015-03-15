509b2b153128d92b29ddd6019b064f75d3e65d51
Stefan Blaginov <stefanblag@gmail.com>
2015-03-15 06:49:34 +0000

    OAuth works correctly and is compartmentalised in the OAuth helper class. It now mainly uses SharedPreferences to store and pass data.

--- app/src/main/java/com/blaginov/mapparatus/MapEditor.java
+++ app/src/main/java/com/blaginov/mapparatus/MapEditor.java
@@ -1,36 +1,25 @@
 package com.blaginov.mapparatus;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.blaginov.mapparatus.util.GetThatToken;
 import com.blaginov.mapparatus.util.OAuthHelper;
 
 import java.io.UnsupportedEncodingException;
-import java.util.concurrent.ExecutionException;
-
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
 
 
 public class MapEditor extends ActionBarActivity {
+    private SharedPreferences sharedPrefs;
     private OAuthHelper oAuth;
-    private final String consKey = "LOzBvclzt3FQjEK9ZJexNzgINUoYe43giLzRV2Va";
-    private final String consSec = "qdv36SIwNkpJuJjgvl7RvPZuJbDdnHPNe0mojiLI";
-    private final String urlBase = "https://www.openstreetmap.org/";
+
+    // To be used once the app is ready for production well
+    //private final String consKeyO = "LOzBvclzt3FQjEK9ZJexNzgINUoYe43giLzRV2Va";
+    //private final String consSecO = "qdv36SIwNkpJuJjgvl7RvPZuJbDdnHPNe0mojiLI";
+    //private final String urlBaseO = "https://www.openstreetmap.org/";
     private final String consKeyD = "CNFPvywkyDrqEaA6CiBgdhLxhaRJgRdHdV146pR1";
     private final String consSecD = "7XQE30u7qeKQpVp0sT4ZRyfsS5UWoQrOtyUyG7md";
     private final String urlBaseD = "http://api06.dev.openstreetmap.org/";
@@ -41,38 +30,25 @@ public class MapEditor extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_editor);
 
+        // Initialise the shared preferences object
+        sharedPrefs = getSharedPreferences("mapparatus", 0);
+
+        setOAuthPreferences(consKeyD, consSecD, urlBaseD, callback);
         try {
-            oAuth = new OAuthHelper(urlBaseD, consKeyD, consSecD, callback);
+            oAuth = new OAuthHelper(sharedPrefs);
         } catch (UnsupportedEncodingException e) {
-            Log.e("balls", "UnsupportedEncodingException, yo!");
             e.printStackTrace();
         }
-    }
 
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        String[] token = getVerifier();
-        SharedPreferences sharedPref = getSharedPreferences("mapparatus",0);
-        Log.i("consToken", sharedPref.getString("consToken",""));
-        Log.i("consTokenSecret", sharedPref.getString("consToken",""));
-        oAuth.resume(urlBaseD, consKeyD, consSecD, sharedPref.getString("consToken",""), sharedPref.getString("consTokenSecret",""), callback);
-
-        if (token != null) {
-            try {
-                String[] accessToken = new GetThatToken().execute(oAuth, token[1]).get();
-                Log.i("ladies", accessToken[0]);
-                Log.i("and getlemen", accessToken[1]);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
+        // Resume OAuth processing
+        oAuth.completeOAuthProcess(this.getIntent().getData());
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -96,36 +72,15 @@ public class MapEditor extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startProcess(View view) {
-        String uri = oAuth.getRequestToken();
-        //consTok = oAuth.getConsToken();
-        //consTokSec = oAuth.getConsTokenSecret();
-
-        SharedPreferences sharedPref = getSharedPreferences("mapparatus",0);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("consToken", oAuth.getConsToken());
-        editor.putString("consTokenSecret", oAuth.getConsTokenSecret());
-        editor.commit();
-        
-        startActivity(new Intent("android.intent.action.VIEW", Uri.parse(uri)));
+    public void osmLogin(MenuItem item) {
+        startActivity(new Intent("android.intent.action.VIEW", Uri.parse(oAuth.getRequestToken())));
     }
-    
-    
-    
-    
-    
 
-    private String[] getVerifier() {
-        // extract the token if it exists
-        Uri uri = this.getIntent().getData();
-        if (uri == null) {
-            return null;
-        }
-
-        String token = uri.getQueryParameter("oauth_token");
-        Log.i("token",token);
-        String verifier = uri.getQueryParameter("oauth_verifier");
-        Log.i("verifier",verifier);
-        return new String[] { token, verifier };
+    private void setOAuthPreferences(String consKey, String consSecret, String urlBase, String callbackUrl) {
+        sharedPrefs.edit().putString("consKey", consKey);
+        sharedPrefs.edit().putString("consSecret", consSecret);
+        sharedPrefs.edit().putString("urlBase", urlBase);
+        sharedPrefs.edit().putString("callbackUrl", callbackUrl);
+        sharedPrefs.edit().commit();
     }
 }
--- app/src/main/java/com/blaginov/mapparatus/util/OAuthHelper.java
+++ app/src/main/java/com/blaginov/mapparatus/util/OAuthHelper.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeoutException;
 import oauth.signpost.OAuth;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
-import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
 import oauth.signpost.exception.OAuthCommunicationException;
@@ -16,13 +15,10 @@ import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.exception.OAuthNotAuthorizedException;
 
-import android.app.Application;
-import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
-
-import com.blaginov.mapparatus.R;
-import com.blaginov.mapparatus.exception.OsmException;
 //import com.blaginov.mapparatus.exception.OsmException;
 
 /**
@@ -36,73 +32,21 @@ public class OAuthHelper {
     private static OAuthProvider sProvider;
     private static String sCallbackUrl;
 
-    /* Not using this for now
-    public OAuthHelper(String osmBaseUrl) throws OsmException {
-        Resources r = Application.mainActivity.getResources();
-        String urls[] = r.getStringArray(R.array.api_urls);
-        String keys[] = r.getStringArray(R.array.api_consumer_keys);
-        String secrets[] = r.getStringArray(R.array.api_consumer_secrets);
-        String oauth_urls[] = r.getStringArray(R.array.api_oauth_urls);
+    private static SharedPreferences sharedPrefs;
+    private static SharedPreferences.Editor sharedPrefsEditor;
 
-        for (int i = 0; i < urls.length; i++) {
-            if (urls[i].equalsIgnoreCase(osmBaseUrl)) {
-                sConsumer = new CommonsHttpOAuthConsumer(keys[i], secrets[i]);
-                Log.d("OAuthHelper", "Using " + osmBaseUrl + "oauth/request_token " + osmBaseUrl + "oauth/access_token " + osmBaseUrl + "oauth/authorize");
-                Log.d("OAuthHelper", "With key " + keys[i] + " secret " + secrets[i]);
-                sProvider = new CommonsHttpOAuthProvider(
-                        oauth_urls[i] + "oauth/request_token",
-                        oauth_urls[i] + "oauth/access_token",
-                        oauth_urls[i] + "oauth/authorize");
-                sProvider.setOAuth10a(true);
-                sCallbackUrl = "vespucci://oauth/"; //OAuth.OUT_OF_BAND; //
-                return;
-            }
-        }
+    public OAuthHelper(SharedPreferences sharedPrefs) throws UnsupportedEncodingException {
+        OAuthHelper.sharedPrefs = sharedPrefs;
+        OAuthHelper.sharedPrefsEditor = sharedPrefs.edit();
 
-        Log.d("OAuthHelper", "No matching API for " + osmBaseUrl + "found");
-        throw new OsmException("No matching OAuth configuration found for this API");
-    }*/
-
-    public OAuthHelper(String osmBaseUrl, String consumerKey, String consumerSecret, String callbackUrl) throws UnsupportedEncodingException {
-        Log.i("consumerKey",consumerKey);
-        Log.i("consumerSecret",consumerSecret);
-        sConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
-        sProvider = new CommonsHttpOAuthProvider(
-                osmBaseUrl + "oauth/request_token",
-                osmBaseUrl + "oauth/access_token",
-                osmBaseUrl + "oauth/authorize");
-        sProvider.setOAuth10a(true);
-        sCallbackUrl = (callbackUrl == null ? OAuth.OUT_OF_BAND : callbackUrl);
+        OAuthHelper.sConsumer = new CommonsHttpOAuthConsumer(sharedPrefs.getString("consKey", ""), sharedPrefs.getString("consSecret", ""));
+        OAuthHelper.sProvider = new CommonsHttpOAuthProvider(
+                sharedPrefs.getString("urlBase", "") + "oauth/request_token",
+                sharedPrefs.getString("urlBase", "") + "oauth/access_token",
+                sharedPrefs.getString("urlBase", "") + "oauth/authorize");
+        OAuthHelper.sProvider.setOAuth10a(true);
+        OAuthHelper.sCallbackUrl = (sharedPrefs.getString("callbackUrl", "") == null ? OAuth.OUT_OF_BAND : sharedPrefs.getString("callbackUrl", ""));
     }
-
-    /**
-     * this constructor is for access to the singletons
-     */
-    public OAuthHelper() {
-    }
-
-    /**
-     * Returns an OAuthConsumer initialized with the consumer keys for the API in question
-     *
-     * @param osmBaseUrl
-     * @return
-     */
-    /* Not for now
-    public OAuthConsumer getConsumer(String osmBaseUrl) {
-        Resources r = Application.mainActivity.getResources();
-
-        String urls[] = r.getStringArray(R.array.api_urls);
-        String keys[] = r.getStringArray(R.array.api_consumer_keys);
-        String secrets[] = r.getStringArray(R.array.api_consumer_secrets);
-        for (int i = 0; i < urls.length; i++) {
-            if (urls[i].equalsIgnoreCase(osmBaseUrl)) {
-                return new DefaultOAuthConsumer(keys[i], secrets[i]);
-            }
-        }
-        Log.d("OAuthHelper", "No matching API for " + osmBaseUrl + "found");
-        // TODO: protect against failure
-        return null;
-    }*/
 
     /**
      * @return null if fails
@@ -121,11 +65,10 @@ public class OAuthHelper {
 
                 try {
                     String authUrl = sProvider.retrieveRequestToken(sConsumer, sCallbackUrl);
-                    Log.i("consumer token",sConsumer.getToken());
-                    Log.i("consumer token secret",sConsumer.getTokenSecret());
-                    Log.i("consumer key",sConsumer.getConsumerKey());
-                    Log.i("consumer secret",sConsumer.getConsumerSecret());
-                    Log.i("provider request token",authUrl);
+                    sharedPrefsEditor.putString("consToken", sConsumer.getToken());
+                    sharedPrefsEditor.putString("consTokenSecret", sConsumer.getTokenSecret());
+                    sharedPrefsEditor.commit();
+
                     return authUrl;
                 } catch (Exception e) {
                     Log.d("Main", "OAuth handshake failed");
@@ -139,20 +82,13 @@ public class OAuthHelper {
                 Log.d("Main", "oAuthHandshake onPostExecute");
                 result = authUrl;
             }
-        };
+        }
 
         MyTask loader = new MyTask();
         loader.execute();
         try {
             return loader.get(10, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            // TODO: Auto-generated catch block
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            // TODO: Auto-generated catch block
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            // TODO: Auto-generated catch block
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
             e.printStackTrace();
         }
         return null;
@@ -160,7 +96,6 @@ public class OAuthHelper {
 
     /**
      * Transforms the verifier string into an access token consisting of a pair of consumer token and secret
-     * @param verifier
      * @return A string array containing the consumer's token and secret
      * @throws OAuthMessageSignerException
      * @throws OAuthNotAuthorizedException
@@ -168,7 +103,6 @@ public class OAuthHelper {
      * @throws OAuthCommunicationException
      */
     public String[] getAccessToken(String verifier) throws OAuthMessageSignerException, OAuthNotAuthorizedException, OAuthExpectationFailedException, OAuthCommunicationException {
-        Log.d("OAuthHelper", "verifier: " + verifier);
 
         // If this helper class is not initialised throw an exception
         if (sProvider == null || sConsumer == null) {
@@ -178,34 +112,37 @@ public class OAuthHelper {
         // Get token from provider
         sProvider.retrieveAccessToken(sConsumer, verifier);
 
-        Log.i("consumer token",sConsumer.getToken());
-        Log.i("consumer access key (secret)",sConsumer.getTokenSecret());
-
         // Return a string array containing the consumer token and secret
         return new String[] {sConsumer.getToken(), sConsumer.getTokenSecret()};
     }
 
-    public void resume(String osmBaseUrl, String consumerKey, String consumerSecret, String requestToken, String tokenSecret, String callbackUrl) {
-        //sConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
-        sConsumer.setTokenWithSecret(requestToken, tokenSecret);
-        //sProvider = new CommonsHttpOAuthProvider(
-               // osmBaseUrl + "oauth/request_token",
-              //  osmBaseUrl + "oauth/access_token",
-              //  osmBaseUrl + "oauth/authorize");
-       // sProvider.setOAuth10a(true);
-        //sCallbackUrl = (callbackUrl == null ? OAuth.OUT_OF_BAND : callbackUrl);
+    public void completeOAuthProcess(Uri uri) {
+        sConsumer.setTokenWithSecret(sharedPrefs.getString("consToken", ""), sharedPrefs.getString("consTokenSecret", ""));
 
-        Log.i("consumer token n",sConsumer.getToken());
-        Log.i("consumer token secret n",sConsumer.getTokenSecret());
-        Log.i("consumer key n",sConsumer.getConsumerKey());
-        Log.i("consumer secret n",sConsumer.getConsumerSecret());
+        if (getVerifier(uri) != null) {
+            try {
+                String[] accessToken = new GetThatToken().execute(this, getVerifier(uri)[1]).get();
+                sharedPrefs.edit().putString("accessToken0", accessToken[0]);
+                sharedPrefs.edit().putString("accessToken1", accessToken[1]);
+
+                Log.i("accessToken0",accessToken[0]);
+                Log.i("accessToken1",accessToken[1]);
+
+                sharedPrefs.edit().commit();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public String getConsToken() {
-        return sConsumer.getToken();
-    }
+    private String[] getVerifier(Uri uri) {
+        // extract the token if it exists
+        if (uri == null) {
+            return null;
+        }
 
-    public String getConsTokenSecret() {
-        return sConsumer.getTokenSecret();
+        String token = uri.getQueryParameter("oauth_token");
+        String verifier = uri.getQueryParameter("oauth_verifier");
+        return new String[] { token, verifier };
     }
 }
